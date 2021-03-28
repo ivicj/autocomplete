c56ebb56298819c88ffde69000c59179294a989c
Jelena Ivic <ivicj123@gmail.com>
2021-03-28 19:00:36 +0200

    order alphabetically asc or desc

--- src/components/Stores.vue
+++ src/components/Stores.vue
@@ -1,9 +1,24 @@
 <template>
   <div>
-    <button @click.prevent="show = 'stores'">Show stores</button>
-    <button @click.prevent="show = 'cities'">Show cities</button>
+    <div>
+      <button @click.prevent="show = 'stores'">Show stores</button>
+      <button @click.prevent="show = 'cities'">Show cities</button>
+    </div>
 
-    <search-autocomplete :items="dynamicStores"  @filteredResults="filteredResults($event)"/>
+    <div v-if="show === 'cities'">
+      <button v-if="sortDirection === 'asc'" v-on:click="sort('asc')">
+        ASC
+      </button>
+      <button v-if="sortDirection === 'desc'" v-on:click="sort('desc')">
+        DESC
+      </button>
+    </div>
+
+    <search-autocomplete
+      v-if="show === 'stores'"
+      :items="dynamicStores"
+      @filteredResults="filteredResults($event)"
+    />
 
     <ul v-if="show === 'stores'">
       <li v-for="store in filteredDynamicStores" :key="store.uuid">
@@ -30,7 +45,9 @@ export default {
       dynamicStores: [],
       dynamicCities: [],
       filteredDynamicStores: [],
+      // sortedDynamicCities: [],
       show: "",
+      sortDirection: "",
       cities: ["Amsterdam", "Veghel"],
     };
   },
@@ -41,21 +58,31 @@ export default {
         .then((data) => {
           this.dynamicStores = data;
           this.filteredDynamicStores = this.dynamicStores;
+          this.sortDirection = "asc";
           this.dynamicCities = data
             .map((store) => store.city)
-            .filter((item, idx, self) => self.indexOf(item) === idx);
+            .filter((item, idx, self) => self.indexOf(item) === idx)
+            .sort((a, b) => a.localeCompare(b));
         });
     },
-    filteredResults (filteredData) {
+    filteredResults(filteredData) {
       var filteredList = [];
-      if(Array.isArray(filteredData)){
+      if (Array.isArray(filteredData)) {
         filteredList = filteredData;
       } else {
-        filteredList.push(filteredData); 
+        filteredList.push(filteredData);
       }
       this.filteredDynamicStores = filteredList;
       return this.filteredDynamicStores;
-   }
+    },
+    sort(sortDirection) {
+      if (sortDirection === "desc") {
+        this.sortDirection = "asc";
+        return this.dynamicCities.sort((a, b) => b.localeCompare(a));
+      }
+      this.sortDirection = "desc";
+      return this.dynamicCities.sort((a, b) => a.localeCompare(b));
+    },
   },
   async created() {
     await this.prepareDynamicStores();
